2a6dba1b13613a87ce80b9f0df77ec7e03536b53
Nigth <omnes.theo@gmail.com>
2022-03-30 12:18:20 +0200

    routes get and post

--- index.ts
+++ index.ts
@@ -9,6 +9,16 @@ app.get('/esgi', (req, res) => {
     res.send("UwU");
 })
 
+app.get('/shool/:name', (req, res) => {
+    const name: string = req.params.name;
+    const {lang} = req.query || 'en';
+    res.send('Name: '+ name + ', lang: ' + lang);
+});
+
+app.post('/oppa', express.json(), (req, res) => {
+    res.json(req.body);
+})
+
 
 app.listen(process.env.PORT, () => {
     console.log('server started on ' + process.env.PORT);
