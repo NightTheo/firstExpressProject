3c05cb9782aa9bceb7c4a0eddaf277b539771f6c
Nigth <omnes.theo@gmail.com>
2022-03-30 11:33:20 +0200

    start server

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("ok");
+import {config} from "dotenv";
+config();
+
+import express from "express";
+
+const app = express();
+
+app.get('/esgi', (req, res) => {
+    res.send("UwU");
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log('server started on ' + process.env.PORT);
+});
